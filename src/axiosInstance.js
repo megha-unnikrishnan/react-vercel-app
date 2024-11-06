2ceeaa467dd6531d4d75f68cd4972f3bce63200e
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-11-06 10:20:59 +0530

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // Django backend URL
+  baseURL: 'http://3.92.22.96', // Django backend URL
 });
 
 axiosInstance.interceptors.request.use(
