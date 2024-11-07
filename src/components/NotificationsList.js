040807c03ed2d41845df5c3c1b43ce3b57e369cb
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-11-07 17:08:39 +0530

    Update NotificationsList.js

--- src/components/NotificationsList.js
+++ src/components/NotificationsList.js
@@ -48,7 +48,7 @@ const NotificationPopover = () => {
   // Setup WebSocket connection for real-time notifications
   useEffect(() => {
     const token = localStorage.getItem('token');
-    const wsUrl = `wss://https://talkstream.xyz/ws/notifications/?token=${token}`;
+    const wsUrl = `wss://talkstream.xyz/ws/notifications/?token=${token}`;
  
     ws.current = new WebSocket(wsUrl);
   
