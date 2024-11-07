1aa3e795d25510dc79683275135ad50d01d39207
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-11-07 15:35:04 +0530

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.92.22.96', // Django backend URL
+  baseURL: 'https://talkstream.xyz', // Django backend URL
 });
 
 axiosInstance.interceptors.request.use(
