7e41dfffea2dabb186757435d397cd0038f259f0
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-11-15 17:44:56 +0530

    Update UserLogin.js

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -290,13 +290,17 @@ const UserLogin = () => {
                         Forgot password?
                     </Link>
                 </div>
-                <div className="mt-6 text-center">
+
+
+                        
+            </div>
+
+                      <div className="mt-6 text-center">
                     <h2 className="text-lg font-semibold text-gray-800 mb-4">Or login with Google</h2>
                     <div className="flex justify-center">
                         <GoogleLoginButton />
                     </div>
                 </div>
-            </div>
 
             {/* Success Popup */}
             {successMessage && (
